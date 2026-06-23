751a4d0e4d7e7f3a84aa04f4ad16b8dfe9a74410
Akash kumar yadav <akashdav3208@gmail.com>
2026-06-23 23:15:54 +0530

    Add files via upload

---
+++ Lab1.3.php
@@ -0,0 +1,11 @@
+<?php
+$a = 10;
+$b = 5;
+
+echo "Addition: " . ($a + $b) . "<br>";
+echo "Subtraction: " . ($a - $b) . "<br>";
+echo "Multiplication: " . ($a * $b) . "<br>";
+echo "Division: " . ($a / $b) . "<br>";
+echo "Equal: " . ($a == $b) . "<br>";
+echo "Greater: " . ($a > $b);
+?>
