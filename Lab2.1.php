335da0d50db175ea56247d43e0e2d90fc70bda7c
Akash kumar yadav <akashdav3208@gmail.com>
2026-06-24 21:14:25 +0530

    Add files via upload

---
+++ Lab2.1.php
@@ -0,0 +1,13 @@
+
+#1. Create a function named collegeInfo() that displays: College Name Course Name Academic Year
+
+<?php
+function collegeInfo()
+{
+    echo "College Name: Marwadi University\n";
+    echo "Course Name: BCA\n";
+    echo "Academic Year: 2025-26";
+}
+
+collegeInfo();
+?>
