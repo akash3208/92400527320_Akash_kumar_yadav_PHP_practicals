d13755f39397f391669175892b81d7b37a0e170f
Akash kumar yadav <akashdav3208@gmail.com>
2026-08-12 21:31:37 +0530

    Delete unit_3 directory

--- unit_3/3.4.php
+++
@@ -1,8 +0,0 @@
-<?php
-// 3.4 Write a PHP script to delete a cookie.
-
-// Delete the cookie by setting its expiry time in the past
-setcookie("username", "", time() - 3600);
-
-echo "Cookie Deleted Successfully!";
-?>
